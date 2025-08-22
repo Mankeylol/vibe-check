e21c967a2b1b0e38ccc9f941dd26d26e81d4427a
mankeylol <mankeyooooo@gmail.com>
2025-08-23 04:44:27 +0530

    minor changes

--- app/components/FetchButton.tsx
+++ app/components/FetchButton.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 export default function FetchButton({
-  onClick,
-  loading,
+    onClick,
+    loading,
 }: {
-  onClick: () => void;
-  loading: boolean;
+    onClick: () => void;
+    loading: boolean;
 }) {
-  return (<>
-  <div className="flex justify-center">
-    <button
-      onClick={onClick}
-      disabled={loading}
-      className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50"
-    >
-      {loading ? "Loading..." : "Fetch Casts"}
-    </button>
-    </div>
+    return (<>
+        <div className="flex justify-center items-center flex-col" >
+            <h1 className="text-lg font-bold mb-[100px]">Check your most engaged Cast</h1>
+            <div>
+                <button
+                    onClick={onClick}
+                    disabled={loading}
+                    className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50"
+                >
+                    {loading ? "Loading..." : "Fetch Casts"}
+                </button>
+            </div>
+        </div>
     </>
-  );
+    );
 }
